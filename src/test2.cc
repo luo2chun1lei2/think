f14d46b3619fee8f95832c314a87ebe6168c5c69
luocl <luochunlei@microtrust.com.cn>
2016-01-18 01:26:12 +0800

    临时提交。

--- src/test2.cc
+++ src/test2.cc
@@ -28,18 +28,19 @@ protected:
 public:
 };
 
-class Teach: public RRelation {
+/**
+ * 所属关系。
+ * 建立一个对象的属性的基本关系。
+ */
+class ROwn: public RRelation {
 private:
 protected:
-	Teacher * pTeacher;
-	Student * pStudent;
+
 public:
-	Teach(Teacher *t, Student *s) {
-		pTeacher = t;
-		pStudent = s;
+	ROwn() {
 	}
 
-	virtual ~Teach() {}
+	virtual ~ROwn() {}
 
     virtual void onNotify(RSubject * subject) {
     }
@@ -47,22 +48,11 @@ public:
 
 int test_2() {
 
-
-	// 建立对象
 	Student * zhangSan = new Student();
 	
 	Teacher * liSi = new Teacher();
 	
-	// 建立相互之间的关系
-	Teach * teach1 = new Teach(liSi, zhangSan);
-	
-	// 可以根据其中一个对象和关系，找到相关的对象。
-	// -可以根据老师找到教导的学生。
-	// -可以根据学生，找到教导他的老师。
-	
-	// 可以根据两人，找到他们之间的关系。
-	// 可以根据关系，找到符合条件的对象对。
-
+	ROwn * teach1 = new ROwn(zhangSan);
 
 	return 0;
 }
